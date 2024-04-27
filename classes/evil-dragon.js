fcb80b3715b8641d1654ae0a52e4f7503d4d2f83
sebastian <stmr.tay@gmail.com>
2024-04-26 23:10:48 -0400

    Implemented EvilDragon class as child of Dragon. It creates an evil dragon with a name, color, an array of bad things it does, and it's arch-nemesis. Added instance method that returns a string desribing each bad thing the dragon will do if you invite them over for dinner. Added instance method that returns a string describing the dragon burning their nemesis.

--- classes/evil-dragon.js
+++ classes/evil-dragon.js
@@ -1,1 +1,38 @@
-// Your code here 
+const Dragon = require("./dragon.js");
+
+class EvilDragon extends Dragon {
+    constructor(name, color, evilDoings, nemesis) {
+        super(name, color);
+        this.evilDoings = evilDoings;
+        this.nemesis = nemesis;
+    }
+
+    dontInviteThemOverForDinner() {
+        return this.evilDoings.forEach(action => {
+            console.log(`${this.name} will ${action}`);
+        })
+    }
+
+    burnsNemesis() {
+        return `${this.name} destroys ${this.nemesis} with fire! WHOOOSH!!!`;
+    }
+}
+
+module.exports = EvilDragon;
+
+/****************************** CONSOLE.LOGS **********************************/
+
+// const enderDragon = new EvilDragon(
+//     "Ender Dragon",
+//     "black", [
+//       "spit a fireball at Steve",
+//       "rule over all the Endermen",
+//       "destroy all blocks in its way"
+//     ],
+//     "Steve"
+//   );
+
+//   console.log(enderDragon);
+//   enderDragon.dontInviteThemOverForDinner();
+//   console.log(enderDragon.breathesFire());
+//   console.log(enderDragon.burnsNemesis());
