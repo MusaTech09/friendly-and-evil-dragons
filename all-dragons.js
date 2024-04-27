4530d9fbd2f5a2b441042abf1455672ffd437026
sebastian <stmr.tay@gmail.com>
2024-04-26 23:23:35 -0400

    Created a new friendly dragon and a new evil dragon. Saved getDragons static method to allDragons variable.

--- all-dragons.js
+++ all-dragons.js
@@ -1,5 +1,23 @@
-// Your code here 
+const FriendlyDragon = require("./classes/friendly-dragon.js");
+const EvilDragon = require("./classes/evil-dragon.js");
+const Dragon = require("./classes/dragon.js");
 
+const falkor = new FriendlyDragon('Falkor', 'white', ['save Atreyu from the swamp', 'save Atreyu from the Nothing', 'scare the local bullies into a dumpster'], 'Bastian');
+
+const smaug = new EvilDragon('Smaug', 'color', ['take over your mountain kingdom', 'steal all of your dwarven gold', 'burn down your floating village'], 'Dwarf King');
+
+const allDragons = Dragon.getDragons(falkor, smaug);
+
+/****************************** CONSOLE.LOGS **********************************/
+// console.log(falkor);
+// falkor.hasLifeGoals();
+// console.log(falkor.breathesFire());
+// console.log(falkor.helpsPeople());
+
+// console.log(smaug);
+// smaug.dontInviteThemOverForDinner();
+// console.log(smaug.breathesFire());
+// console.log(smaug.burnsNemesis());
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
@@ -7,6 +25,7 @@
 try {
   module.exports.falkor = falkor
   module.exports.smaug = smaug
+  module.exports.allDragons = allDragons
 } catch {
   module.exports = null;
 }
