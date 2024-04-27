dc7e1094537a5ac290a12622977483566e121567
sebastian <stmr.tay@gmail.com>
2024-04-26 22:34:49 -0400

    Added instance method that returns a string describing a dragon vreathing fire. Added static method that accepts any number of dragon instances and returns an array with all of their names.

--- classes/dragon.js
+++ classes/dragon.js
@@ -1,11 +1,35 @@
-// Your code here
-
 class Dragon {
   constructor(name, color) {
     this.name = name;
     this.color = color;
   }
+
+  breathesFire() {
+    return `${this.name} breathes fire everywhere! BURN!!!!`;
+  }
+
+  static getDragons(...dragons) {
+    let res = [];
+    for(const dragon of dragons) {
+      res.push(dragon.name);
+    }
+
+    return res;
+  }
 }
+
+/****************************** CONSOLE.LOGS ********************************/
+
+
+// const puff = new Dragon("Puff", "green");
+// console.log(puff);
+// console.log(puff.breathesFire());
+
+// const toothless = new Dragon("Toothless", "black");
+// console.log(toothless);
+// console.log(toothless.breathesFire());
+
+// console.log(Dragon.getDragons(puff, toothless));
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
